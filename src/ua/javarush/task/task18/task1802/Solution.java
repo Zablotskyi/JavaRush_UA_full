b0d14cc7a2edb86c643bdb4687988b5e8f897efe
Zablotskyi <zablotskyivv@gmail.com>
2024-03-25 20:04:54 +0200

    completed the task1802 Мінімальний байт

--- src/ua/javarush/task/task18/task1802/Solution.java
+++ src/ua/javarush/task/task18/task1802/Solution.java
@@ -1,8 +1,7 @@
 package ua.javarush.task.task18.task1802;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -11,6 +10,17 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        FileInputStream fis = new FileInputStream(fileName);
+        byte[] array = new byte[fis.available()];
 
+        fis.read(array);
+        Arrays.sort(array);
+        if (scanner != null && fis != null) {
+            scanner.close();
+            fis.close();
+        }
+        System.out.println(array[0]);
     }
 }
