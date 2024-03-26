3b1c5cb082926d9a3f2a7091c4968db438de74d9
Zablotskyi <zablotskyivv@gmail.com>
2024-03-26 18:11:34 +0200

    completed the task1807 Підрахунок ком.

--- src/ua/javarush/task/task18/task1807/Solution.java
+++ src/ua/javarush/task/task18/task1807/Solution.java
@@ -1,9 +1,7 @@
 package ua.javarush.task.task18.task1807;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 /* 
 Підрахунок ком.
@@ -11,5 +9,33 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        FileInputStream fis = null;
+        byte[] array;
+
+        try {
+            fis = new FileInputStream(fileName);
+            array = new byte[fis.available()];
+            fis.read(array);
+            int count = 0;
+            for (Byte item : array) {
+                if (item == 44)
+                    count++;
+            }
+            System.out.println(count);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (scanner != null && fis != null) {
+                scanner.close();
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
