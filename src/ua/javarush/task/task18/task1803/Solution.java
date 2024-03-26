93e4606d33634a5edd8765308dcaf670fde0e396
Zablotskyi <zablotskyivv@gmail.com>
2024-03-26 11:53:29 +0200

    created f.txt file

--- src/ua/javarush/task/task18/task1803/Solution.java
+++ src/ua/javarush/task/task18/task1803/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /* 
 Найчастіші байти
@@ -11,5 +12,14 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        FileInputStream fis = new FileInputStream(fileName);
+        byte[] array = new byte[fis.available()];
+
+        fis.read(array);
+        byte[] tmpArray = array;
+
+
     }
 }
