d759ff7a01185b041b9674107eb707d6dd62a966
Zablotskyi <zablotskyivv@gmail.com>
2024-03-27 16:50:45 +0200

    completed the task1810 DownloadException

--- src/ua/javarush/task/task18/task1810/Solution.java
+++ src/ua/javarush/task/task18/task1810/Solution.java
@@ -1,9 +1,7 @@
 package ua.javarush.task.task18.task1810;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 /* 
 DownloadException
@@ -11,7 +9,29 @@ DownloadException
 
 public class Solution {
     public static void main(String[] args) throws DownloadException {
+        boolean isSmallFile = false;
+        Scanner scanner = new Scanner(System.in);
+        String fileName;
+        byte[] array;
+        FileInputStream fis = null;
 
+        try {
+            while (!isSmallFile) {
+                fileName = scanner.nextLine();
+                fis = new FileInputStream(fileName);
+                if (fis.available() < 1000)
+                    isSmallFile = true;
+                array = new  byte[fis.available()];
+                fis.read(array);
+            }
+            scanner.close();
+            fis.close();
+            throw new DownloadException();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static class DownloadException extends Exception {
