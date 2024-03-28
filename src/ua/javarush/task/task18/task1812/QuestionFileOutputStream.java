2c3db9eb8a90264569c5898892b4ee816f181a74
Zablotskyi <zablotskyivv@gmail.com>
2024-03-28 12:18:57 +0200

    started to perform the task1812

---
+++ src/ua/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.task18.task1812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Розширюємо AmigoOutputStream
+*/
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+
+}
+
