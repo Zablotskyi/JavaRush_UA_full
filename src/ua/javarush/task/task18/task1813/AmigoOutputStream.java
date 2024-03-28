5f7e8a3d403bbd4be857bafd0a06f1cda7094da5
Zablotskyi <zablotskyivv@gmail.com>
2024-03-28 15:00:05 +0200

    completed the task1813 AmigoOutputStream

--- src/ua/javarush/task/task18/task1813/AmigoOutputStream.java
+++ src/ua/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -8,11 +8,48 @@ import java.io.IOException;
 AmigoOutputStream
 */
 
-public class AmigoOutputStream {
+public class AmigoOutputStream extends FileOutputStream {
     public static String fileName = "C:/tmp/result.txt";
+    private FileOutputStream fileOutputStream = new FileOutputStream(fileName, true);
+
+    public AmigoOutputStream(FileOutputStream fileOutputStream) throws FileNotFoundException {
+        super(fileName, true);
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
 
+    public AmigoOutputStream(String name) throws FileNotFoundException {
+        super(name);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.flush();
+        String str = "JavaRush © All rights reserved.";
+        byte[] array = str.getBytes();
+        write(array);
+        fileOutputStream.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
 }
