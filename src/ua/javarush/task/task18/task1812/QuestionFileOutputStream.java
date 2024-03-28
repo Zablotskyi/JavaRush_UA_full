6d1a689ec101320f0ebe7d69c7c8ef99cbb4c2c3
Zablotskyi <zablotskyivv@gmail.com>
2024-03-28 13:11:10 +0200

    completed the task1812 Розширюємо AmigoOutputStream

--- src/ua/javarush/task/task18/task1812/QuestionFileOutputStream.java
+++ src/ua/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -3,12 +3,46 @@ package ua.javarush.task.task18.task1812;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Розширюємо AmigoOutputStream
 */
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
+    private final AmigoOutputStream amigoOutputStream;
 
+    public QuestionFileOutputStream(AmigoOutputStream amigoOutputStream) {
+        this.amigoOutputStream = amigoOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Ви дійсно хочете закрити потік? Т/Н");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        if ("Т".equals(answer)) {
+            amigoOutputStream.close();
+        }
+    }
 }
-
