561710a2ba3c61cc507849da973d39efcdc6e256
Zablotskyi <zablotskyivv@gmail.com>
2024-04-11 18:32:47 +0300

    started to perform the task1817

---
+++ src/ua/javarush/task/task18/task1817/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.task18.task1817;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/* 
+Прогалини
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
