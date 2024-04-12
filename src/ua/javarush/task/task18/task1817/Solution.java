333148468d7c0ef507a4f5dcde5f474a6fecf4dc
Zablotskyi <zablotskyivv@gmail.com>
2024-04-12 23:56:55 +0300

    for verification, he created a text file for the task1817 and passed its address to the program in the parameters

--- src/ua/javarush/task/task18/task1817/Solution.java
+++ src/ua/javarush/task/task18/task1817/Solution.java
@@ -1,7 +1,10 @@
 package ua.javarush.task.task18.task1817;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /* 
 Прогалини
@@ -9,6 +12,32 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) {
+        FileInputStream fis = null;
+        byte[] buffer;
 
+        try {
+            fis = new FileInputStream(args[0]);
+            buffer = new byte[fis.available()];
+            fis.read(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int countSpace = 0;
+        for (Byte item : buffer) {
+            if (item.intValue() == 32)
+                countSpace++;
+        }
+        int countAllSymbols = buffer.length - countSpace;
+        if (fis != null) {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        BigDecimal ratio = new BigDecimal(countSpace / countAllSymbols * 100);
+        BigDecimal result = ratio.setScale(2, RoundingMode.HALF_UP);
+        System.out.println(result);
     }
 }
