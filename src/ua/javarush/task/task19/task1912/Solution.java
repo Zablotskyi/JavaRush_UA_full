75e6cffa41ad0d43560680d4755d1d93394ff8fe
Zablotskyi <zablotskyivv@gmail.com>
2024-04-22 16:30:50 +0300

    started to perform the task1913

--- src/ua/javarush/task/task19/task1912/Solution.java
+++ src/ua/javarush/task/task19/task1912/Solution.java
@@ -11,6 +11,14 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        StringBuilder stringBuilder = new StringBuilder("it's a ??xt for ??sting");
+        System.setOut(consoleStream);
+        System.out.println(stringBuilder);
     }
 
     public static class TestString {
