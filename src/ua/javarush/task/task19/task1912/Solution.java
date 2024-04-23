018cb71926431239fac1a1813ff0827dfd9321e5
Zablotskyi <zablotskyivv@gmail.com>
2024-04-23 17:31:14 +0300

    completed the task1912 Рідер-обгортка 2

--- src/ua/javarush/task/task19/task1912/Solution.java
+++ src/ua/javarush/task/task19/task1912/Solution.java
@@ -13,12 +13,17 @@ public class Solution {
     public static void main(String[] args) {
         PrintStream consoleStream = System.out;
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        System.setOut(printStream);
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
         testString.printSomething();
-        StringBuilder stringBuilder = new StringBuilder("it's a ??xt for ??sting");
+
+        String result = outputStream.toString();
         System.setOut(consoleStream);
-        System.out.println(stringBuilder);
+        StringBuilder stringBuilder = new StringBuilder(result);
+
+        String newString = result.replace("te", "??");
+        System.out.println(newString);
     }
 
     public static class TestString {
