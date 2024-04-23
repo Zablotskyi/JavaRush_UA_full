97721942ebad726c86d968675d3dfd1f436bbae9
Zablotskyi <zablotskyivv@gmail.com>
2024-04-23 17:33:21 +0300

    started to perform the task1911

---
+++ src/ua/javarush/task/task19/task1911/Solution.java
@@ -0,0 +1,22 @@
+package ua.javarush.task.task19.task1911;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Рідер обгортка
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
