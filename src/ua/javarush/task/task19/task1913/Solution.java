b237d38ede3f5e293425f7797e50b811acf6daf7
Zablotskyi <zablotskyivv@gmail.com>
2024-04-23 17:20:54 +0300

    completed the task1913 Виводимо лише цифри

--- src/ua/javarush/task/task19/task1913/Solution.java
+++ src/ua/javarush/task/task19/task1913/Solution.java
@@ -2,6 +2,8 @@ package ua.javarush.task.task19.task1913;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Виводимо лише цифри
@@ -11,6 +13,23 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+
+        StringBuilder stringBuilder = new StringBuilder(result);
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(stringBuilder );
+
+        while (matcher.find()) {
+            System.out.print(matcher.group());
+        }
     }
 
     public static class TestString {
