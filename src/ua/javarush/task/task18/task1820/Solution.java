50eca499508cade8c9d1afafbb0ffef26a3fc207
Zablotskyi <zablotskyivv@gmail.com>
2024-05-01 22:36:03 +0300

    in task 1820, he implemented reading from a halyard. TODO: implement reading real numbers and rounding them to integers. Then write to the file

--- src/ua/javarush/task/task18/task1820/Solution.java
+++ src/ua/javarush/task/task18/task1820/Solution.java
@@ -1,6 +1,8 @@
 package ua.javarush.task.task18.task1820;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /* 
 Округлення чисел
@@ -8,6 +10,22 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstFileName = scanner.nextLine();
+        String secondFileName = scanner.nextLine();
+        try {
+            File file = new File(firstFileName);
+            FileOutputStream fos = new FileOutputStream(secondFileName);
+            Scanner readNumbers = new Scanner(file);
 
+            while (readNumbers.hasNextDouble()) {
+                double number = readNumbers.nextDouble();
+                System.out.println(number);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
