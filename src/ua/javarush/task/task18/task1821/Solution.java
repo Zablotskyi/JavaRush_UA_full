702199a60bf35f0ccbf65aa63c6ae7c4f6acd5e4
Zablotskyi <zablotskyivv@gmail.com>
2024-05-10 23:33:57 +0300

    in the task task1821 added reading from a file, and adding to the TreeMap as keys - a symbol, and as values - the number of their repetitions

--- src/ua/javarush/task/task18/task1821/Solution.java
+++ src/ua/javarush/task/task18/task1821/Solution.java
@@ -1,7 +1,10 @@
 package ua.javarush.task.task18.task1821;
 
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 /* 
 Зустрічаємо символи
@@ -9,6 +12,34 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) {
+        FileInputStream fis = null;
+        Map<Integer, Integer> symbols = new TreeMap<>();
+        try {
+            fis = new FileInputStream(args[0]);
+            int[] buffer = new int[256];
+            while (fis.available() > 0) {
+                buffer[fis.read()] ++;
+            }
+            for (int i = 0; i < buffer.length; i++) {
+                symbols.put(i, buffer[i]);
+            }
+            for (Map.Entry<Integer, Integer> entry : symbols.entrySet()) {
+                char symbol = Character.forDigit(entry.getKey(), 10);
+                if (entry.getValue() != 0)
+                System.out.println(symbol + " " + entry.getValue());
+            }
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (fis != null) {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
